791b1b43c6aaabace2fd85a08f6329d5bcfd1a3c
Vadim Kuznetsov <vadkuze@gmail.com>
2017-09-30 23:00:55 +0300

    app is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { ImageListComponent } from './image-list/image-list.component';
+import { ImageService } from './shared/images.service';
+import { FormsModule } from '@angular/forms';
 
+import { MaterialModule } from '@angular/material';
+import { MasonryModule } from 'angular2-masonry';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule, 
+    FormsModule,
+    MaterialModule,
+    MasonryModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [ImageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  PIXABAY_API_KEY: '6570415-db11f62f0dbb9e0e21636e381',
+  PIXABAY_API_URL: 'https://pixabay.com/api/?key='
 };
